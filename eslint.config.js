572b97ac127a7b10f7629944ddf7181a02db5eff
hntuan7684 <tuanhnt7684@gmail.com>
2025-05-27 11:10:04 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+
+  // Add prettier config last to override ESLint formatting rules
+  eslintConfigPrettier
+]);
